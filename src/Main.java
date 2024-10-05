21ef01079747eb8051cfb4e17f0d2e91b4716d06
Thiago <thiagodeboliveira@outlook.com>
2024-10-05 01:06:55 -0300

    Dia 1 aprendendo Java(iniciando com variaveis e operadores)

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Cyber Teather");
+        System.out.println("Ted 2");
+
+        int ano = 2015;
+        System.out.println("Ano de lançamento:" + ano);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme  = 8.2;
+
+        double media = (10 + 6.3 + 5.8) / 3;
+        System.out.println(media    );
+    }
+}
